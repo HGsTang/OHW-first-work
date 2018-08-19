71d5aecb9f13a055120adc2661d41feacffe572c
HGsTang <you@example.com>
2018-08-19 23:41:41 +0800

    unitTEST

---
+++ 生日查询与星座运势/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("62af74a8-69e2-43ac-a80a-db6c4c2c9ce3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 生日查询与星座运势/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TesttheDayBefore()
+        {
+            var daybefore = 生日查询与星座运势.Form1.theDayBefore(2, 20);
+            Assert.AreEqual(51,daybefore);
+        }
+        [TestMethod]
+        public void TesttheDayafter()
+        {
+            var dayafter = 生日查询与星座运势.Form1.theDayAfter(10, 20);
+            Assert.AreEqual(72, dayafter);
+        }
+        [TestMethod]
+        public void Testnumber()
+        {
+            var daynum = 生日查询与星座运势.Form1.DayNumber(8, 19);
+            Assert.AreEqual(9, daynum);
+        }
+    }
+}
