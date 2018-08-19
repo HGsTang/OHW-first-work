6966439f3a21d58d5a328f102be37aaebd24026b
HGsTang <you@example.com>
2018-08-19 19:58:27 +0800

    add zhushi

--- 生日查询与星座运势/生日查询与星座运势/Form1.cs
+++ 生日查询与星座运势/生日查询与星座运势/Form1.cs
@@ -24,7 +24,7 @@ namespace 生日查询与星座运势
 
         public static int[] months = new int[12] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-        public static int theDayBefore(int month,int day)
+        public static int theDayBefore(int month,int day)//在不考虑闰年情况下当年某天前有多少日子
         {
             int thedaybefore = 0;
             for(int i=0;i<month-1;i++)
@@ -35,7 +35,7 @@ namespace 生日查询与星座运势
             return thedaybefore;
         }
 
-        public static int theDayAfter(int month, int day)
+        public static int theDayAfter(int month, int day)//在不考虑闰年情况下当年某天后有多少日子
         {
             int thedayafter = 0;
             for (int i = month; i < 12; i++)
@@ -47,7 +47,7 @@ namespace 生日查询与星座运势
             return thedayafter;
         }
 
-        public static int theDayBetween(int month1,int day1,int month2,int day2)
+        public static int theDayBetween(int month1,int day1,int month2,int day2)//两天之间有多少日子
         {
             int thedaybetween = 0;
             for (int i = month1; i < month2-1; i++)
@@ -65,7 +65,7 @@ namespace 生日查询与星座运势
             return thedaybetween;
         }
 
-        public static bool Judge(int year)
+        public static bool Judge(int year)//判断闰年与否
         {
             bool judge=false;
             if((year%4==0&&year%100!=0)||(year%400==0))
@@ -74,7 +74,7 @@ namespace 生日查询与星座运势
             }
             return judge;
         }
-        public static int Span(int year, int month, int day)
+        public static int Span(int year, int month, int day)//考虑闰年后两天之间的天数
         {
             int datespan = 0;
             int yearNow = DateTime.Now.Year;
@@ -117,7 +117,7 @@ namespace 生日查询与星座运势
             return datespan;
         }
 
-        public static int theNextBirthday(int month, int day)
+        public static int theNextBirthday(int month, int day)//下一个生日还有多少天
         {
             int yearNow = DateTime.Now.Year;
             int monthNow = DateTime.Now.Month;
@@ -174,7 +174,7 @@ namespace 生日查询与星座运势
             return thenextspan;
         }
 
-        public static string Constellation(int month3,int day3)
+        public static string Constellation(int month3,int day3)//判断星座
         {
             string result;
             if(month3==1)
@@ -264,7 +264,7 @@ namespace 生日查询与星座运势
             return result;
         }
 
-        public static int DayNumber(int month, int day)
+        public static int DayNumber(int month, int day)//计算出一个对应某天的数字
         {
             int daynumber = month / 10 + month % 10 + day / 10 + day % 10;
             do
@@ -274,7 +274,7 @@ namespace 生日查询与星座运势
             return daynumber;
         }
 
-        public static int LuckyNumber(int month1, int day1, int month2, int day2)
+        public static int LuckyNumber(int month1, int day1, int month2, int day2)//利用每天的对应数字计算幸运数字
         {
             int number1 = DayNumber(month1, day1);
             int number2 = DayNumber(month2, day2);
