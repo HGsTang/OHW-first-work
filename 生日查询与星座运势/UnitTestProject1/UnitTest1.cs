c5032b3a9826af9bfdf2878970cfe7e1ddfeb1f9
SydneyRaymond <782602094@qq.com>
2018-08-20 09:39:38 +0800

    add yu

--- 生日查询与星座运势/UnitTestProject1/UnitTest1.cs
+++ 生日查询与星座运势/UnitTestProject1/UnitTest1.cs
@@ -36,5 +36,25 @@ namespace UnitTestProject1
             var result = 生日查询与星座运势.Form2.FortuneToday(8);
             Assert.AreEqual(result, "爱他就要说出来，你不说他（她）怎么会知道呢？宜放手去爱不要逃。");
         }
+        [TestMethod]
+        public void Testlucky_num()
+        {
+
+            int month1 = 8, day1 = 19, month2 = 8, day2 = 20;
+            int result = 生日查询与星座运势.Form1.LuckyNumber(month1, day1, month2, day2);//幸运数字
+            //int result = 生日查询与星座运势.Form1.Span(year, month, day);
+            Assert.AreEqual(8, result);
+        }
+        [TestMethod]
+        public void Testspan()
+        {
+           
+            int yearNow = DateTime.Now.Year;
+            int monthNow = DateTime.Now.Month;
+            int dayNow = DateTime.Now.Day;
+            int year = yearNow-1, month = monthNow, day = dayNow;
+            int result = 生日查询与星座运势.Form1.Span(year, month, day);
+            Assert.AreEqual(365, result);
+        }
     }
 }
